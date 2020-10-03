16f7c18874d36a6c50398f615f9c81c7f4aa0194
Dan <link.dohi@gmail.com>
2020-10-03 00:19:35 -0700

    Tweaked egg physics

--- egg.gd
+++ egg.gd
@@ -12,13 +12,14 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-		if Input.is_action_just_pressed("egg_toggle"):
-			var new_player = player.instance()
-			new_player.position = position
-			new_player.velocity = linear_velocity
-			get_parent().add_child(new_player)
-			.hide()
-			.queue_free()
+	angular_velocity = max(linear_velocity.x / 20, angular_velocity)
+	if Input.is_action_just_pressed("egg_toggle"):
+		var new_player = player.instance()
+		new_player.position = position
+		new_player.velocity = linear_velocity
+		get_parent().add_child(new_player)
+		.hide()
+		.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
